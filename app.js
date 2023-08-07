a7ccedb7d724b70a48752d1159d2e69c7019b112
vulebaolong <vulebaolong@gmail.com>
2023-08-07 21:50:48 +0700

    origin

--- app.js
+++ app.js
@@ -3,22 +3,21 @@ const express = require("express");
 const routers = require("./src/routers");
 const cors = require("cors");
 
-
 const app = express();
 
 // Danh sách các đường dẫn được cho phép truy cập
-const allowedOrigins = ['https://netflix-vulebaolong.netlify.app'];
+const allowedOrigins = ["https://netflix-vulebaolong.netlify.app"];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    console.log(origin);
-    // Kiểm tra xem origin có trong danh sách allowedOrigins hay không
-    if (origin === undefined || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true); // Cho phép truy cập
-    } else {
-      callback(new Error('Không cho phép truy cập từ nguồn này')); // Từ chối truy cập
-    }
-  },
+    origin: function (origin, callback) {
+        console.log("origin: ", origin);
+        // Kiểm tra xem origin có trong danh sách allowedOrigins hay không
+        if (origin === undefined || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true); // Cho phép truy cập
+        } else {
+            callback(new Error("Không cho phép truy cập từ nguồn này")); // Từ chối truy cập
+        }
+    },
 };
 
 app.use(cors(corsOptions));
