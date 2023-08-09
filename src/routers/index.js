432c68299f44c4391da0f6fcbf937f0ea7d3f32c
vulebaolong <vulebaolong@gmail.com>
2023-08-09 11:23:38 +0700

    change welcome

--- src/routers/index.js
+++ src/routers/index.js
@@ -12,7 +12,7 @@ const router = express.Router();
 // health check
 router.get("/welcome", (req, res) => {
     // console.log("health check");
-    res.status(200).json("OK");
+    res.status(200).json("Welcome Api Movie vulebaolong");
 });
 
 router.use("/QuanLyNguoiDung", quanLyNguoiDungRouter);
